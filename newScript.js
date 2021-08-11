c7e090e23136ec6c34a5cfff62b2c9950916da27
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2021-08-12 00:50:09 +0500

    Change speaker info using DOM

--- newScript.js
+++ newScript.js
@@ -13,6 +13,7 @@ const openMain = document.querySelector('.home-link');
 const openSponsor = document.querySelector('.partner-link');
 const home = document.getElementById('home');
 const about = document.querySelector('.about');
+let numv = 3;
 
 function openMenu() {
   overlay.classList.remove('hidden');
@@ -55,66 +56,105 @@ for (let i = 0; i < options.length; i += 1) {
 
 // Hamburger Button end //////////////////
 
-const data = {
-  speaker1: {
-    img: 'img/speakers/Hasan-Mamasaidov.png',
-    name: 'Hasanfdsa Mamasaidov',
-    role: 'Founder of "Dekos.uz',
-    info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat recusandae molestias cumque totam aliquam mollitia, eligendi distinctio',
-  },
-
-  speaker2: {
+const data = [
+  {
     img: 'img/speakers/Hasan-Mamasaidov.png',
     name: 'Hasan Mamasaidov',
-    role: 'Founder of "Dekos.uz',
+    rule: 'Founder of "Dekos.uz',
     info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat recusandae molestias cumque totam aliquam mollitia, eligendi distinctio',
   },
-
-  speaker3: {
-    img: 'img/speakers/Hasan-Mamasaidov.png',
-    name: 'Hasan Mamasaidov',
-    role: 'Founder of "Dekos.uz',
+  {
+    img: 'img/speakers/Hikmat-Abdurahmonov.jpeg',
+    name: 'Hikmat Abdurahmonov',
+    rule: 'Founder of "HM Partners, Technoplaza, GroundZero"',
     info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat recusandae molestias cumque totam aliquam mollitia, eligendi distinctio',
   },
-
-  speaker4: {
-    img: 'img/speakers/Hasan-Mamasaidov.png',
-    name: 'Hasan Mamasaidov',
-    role: 'Founder of "Dekos.uz',
+  {
+    img: 'img/speakers/Muhammad-Ali-Eshounqulov.jpeg',
+    name: 'Muhammad Ali Eshonqulov',
+    rule: 'International-class economist, business coach, founder of the Patrons League.',
     info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat recusandae molestias cumque totam aliquam mollitia, eligendi distinctio',
   },
-
-  speaker5: {
-    img: 'img/speakers/Hasan-Mamasaidov.png',
-    name: 'Hasan Mamasaidov',
-    role: 'Founder of "Dekos.uz',
+  {
+    img: 'img/speakers/Feruzbek-Aliev.jpeg',
+    name: 'Feruzbek Aliev',
+    rule: 'Founder of "Asaxiy.uz',
     info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat recusandae molestias cumque totam aliquam mollitia, eligendi distinctio',
   },
-
-  speaker6: {
-    img: 'img/speakers/Hasan-Mamasaidov.png',
-    name: 'Hasan Mamasaidov',
-    role: 'Founder of "Dekos.uz',
+  {
+    img: 'img/speakers/Temurbek-Adhamov.jpeg',
+    name: 'Temurbek Adhamov',
+    rule: 'Founder of "Najot Talim"',
     info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat recusandae molestias cumque totam aliquam mollitia, eligendi distinctio',
   },
-};
+  {
+    img: 'img/speakers/Shahzod-Bahodirov.jpeg',
+    name: 'Shahzod Bahodirov',
+    rule: 'Founder of "Thompson School"',
+    info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat recusandae molestias cumque totam aliquam mollitia, eligendi distinctio',
+  },
+];
 
-// // const nameID = document.getElementById('name1');
-// const img = document.getElementById('img');
-// const nameID = document.getElementById('name');
-// const rule = document.getElementById('rule');
-// const info = document.getElementById('info');
-// // const speaker = data[`speaker${order}`];
-// // img.src = data.speaker.img;
-// // rule.innerHTML = data.speaker.rule;
-// // info.innerHTML = data.speaker.info;
+function changeCard(num) {
+  const img = document.querySelectorAll('#img')[num];
+  const nameID = document.querySelectorAll('#name')[num];
+  const rule = document.querySelectorAll('#rule')[num];
+  const info = document.querySelectorAll('#info')[num];
+  img.src = data[num].img;
 
-// function changePopup(order) {
-//   const speaker = data[`speaker${order}`];
-//   img.src = speaker.img;
-//   nameID.innerHTML = speaker.name;
-//   rule.innerHTML = speaker.rule;
-//   info.innerHTML = speaker.info;
-// }
+  nameID.innerHTML = data[num].name;
+  img.src = data[num].img;
+  rule.innerHTML = data[num].rule;
+  info.innerHTML = data[num].info;
+}
 
-// `nameID.innerHTML = 'Paragraph changed!';
+function smth() {
+  if (window.innerWidth < 768) {
+    // document.querySelector('more').addEventListener('click', (num = 6));
+
+    for (let i = 0; i < numv; i += 1) {
+      const speaker = `
+            <div class="speaker">
+            <img id="img" src="img/speakers/Hasan-Mamasaidov.png" alt="speaker" class="speaker-img" />
+            <div class="speaker-text">
+            <h2 id="name" class="speaker-name"></h2>
+              <p id="rule" class="speaker-job"></p>
+              <div class="line"></div>
+              <p id="info" class="speaker-info"></p>
+            </div>
+          </div>
+`;
+
+      document.getElementById('speaker-card').innerHTML += speaker;
+
+      changeCard(i);
+    }
+  }
+}
+
+smth();
+// MOBILE MORE OPTION ///////////
+
+const btnMore = document.querySelector('.button-more');
+const btnLess = document.querySelector('.button-less');
+btnLess.classList.add('hidden');
+
+btnMore.addEventListener('click', () => {
+  document.getElementById('speaker-card').innerHTML = '';
+  // const url = 'https://bit.ly/IqT6zt';
+  // window.open(url, '_blank').focus();
+  btnMore.classList.add('hidden');
+  btnLess.classList.remove('hidden');
+  numv = 6;
+  smth();
+});
+
+btnLess.addEventListener('click', () => {
+  document.getElementById('speaker-card').innerHTML = '';
+  // const url = 'https://bit.ly/IqT6zt';
+  // window.open(url, '_blank').focus();
+  btnLess.classList.add('hidden');
+  btnMore.classList.remove('hidden');
+  numv = 2;
+  smth();
+});
